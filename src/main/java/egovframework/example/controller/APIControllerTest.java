f37be025c9a44cd7fbbe6d810b2966a95d0afbf8
Zino <jinho1203@gmail.com>
2025-07-15 17:45:35 +0900

    API TEST3
    인아이티 , API 하드코딩 연습

--- src/main/java/egovframework/example/controller/APIControllerTest.java
+++ src/main/java/egovframework/example/controller/APIControllerTest.java
@@ -16,6 +16,7 @@ public class APIControllerTest {
 	@GetMapping("/api/test")
 	public String callApi() throws IOException{
 		
+		/*
 		StringBuilder urlBuilder = new StringBuilder("https://kosis.kr/openapi/Param/statisticsParameterData.do");
 		
 		urlBuilder.append("?" + URLEncoder.encode("method","UTF-8") + "=" + URLEncoder.encode("getList", "UTF-8"));
@@ -38,10 +39,34 @@ public class APIControllerTest {
         urlBuilder.append("&" + URLEncoder.encode("newEstPrdCnt","UTF-8") + "=" + URLEncoder.encode("3", "UTF-8")); 
         urlBuilder.append("&" + URLEncoder.encode("orgId","UTF-8") + "=" + URLEncoder.encode("101", "UTF-8")); 
         urlBuilder.append("&" + URLEncoder.encode("tblId","UTF-8") + "=" + URLEncoder.encode("INH_1C91", "UTF-8")); 
+        */
+		
+		StringBuilder urlBuilder = new StringBuilder("https://kosis.kr/openapi/Param/statisticsParameterData.do");
+		
+		urlBuilder.append("?" + URLEncoder.encode("method","UTF-8") + "=" + URLEncoder.encode("getList", "UTF-8"));
+		urlBuilder.append("&" + URLEncoder.encode("apiKey","UTF-8") + "=ZGFlYjQxMTg4NTM0OTAxZDFjNzJiN2IxMDI5MjQwNjE="); 
+		
+		String itmIdRaw = "13103134767999+";
+        urlBuilder.append("&" + URLEncoder.encode("itmId","UTF-8") + "=" + URLEncoder.encode(itmIdRaw, "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("objL1", "UTF-8") + "=" + URLEncoder.encode("ALL", "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("objL2", "UTF-8") + "=");
+        urlBuilder.append("&" + URLEncoder.encode("objL3", "UTF-8") + "=");
+        urlBuilder.append("&" + URLEncoder.encode("objL4", "UTF-8") + "=");
+        urlBuilder.append("&" + URLEncoder.encode("objL5", "UTF-8") + "=");
+        urlBuilder.append("&" + URLEncoder.encode("objL6", "UTF-8") + "=");
+        urlBuilder.append("&" + URLEncoder.encode("objL7", "UTF-8") + "=");
+        urlBuilder.append("&" + URLEncoder.encode("objL8", "UTF-8") + "=");
+        urlBuilder.append("&" + URLEncoder.encode("format","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); 
+        urlBuilder.append("&" + URLEncoder.encode("jsonVD","UTF-8") + "=" + URLEncoder.encode("Y", "UTF-8")); 
+        urlBuilder.append("&" + URLEncoder.encode("prdSe","UTF-8") + "=" + URLEncoder.encode("M", "UTF-8")); 
+        urlBuilder.append("&" + URLEncoder.encode("newEstPrdCnt","UTF-8") + "=" + URLEncoder.encode("3", "UTF-8")); 
+        urlBuilder.append("&" + URLEncoder.encode("orgId","UTF-8") + "=" + URLEncoder.encode("101", "UTF-8")); 
+        urlBuilder.append("&" + URLEncoder.encode("tblId","UTF-8") + "=" + URLEncoder.encode("DT_1YL7101E", "UTF-8")); 
         
         URL url = new URL(urlBuilder.toString());
         
         System.out.println("url : " + url);
+        System.out.println("itmIdRaw : " + itmIdRaw);
         
         HttpURLConnection conn = (HttpURLConnection)url.openConnection();
         
