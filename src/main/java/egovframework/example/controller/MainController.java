1c0654f62c5d5ec5c1ffac9709582ea81c4464f5
Zino <jinho1203@gmail.com>
2025-07-22 14:19:19 +0900

    Project 등록
    버튼 클릭시 등록 - DB도 성공

--- src/main/java/egovframework/example/controller/MainController.java
+++ src/main/java/egovframework/example/controller/MainController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import egovframework.example.service.ProjectService;
 import egovframework.example.vo.ProjectVO;
@@ -32,12 +35,20 @@ public class MainController {
     }
     
     @PostMapping("/project")
-    public String insertProject(Model model, @RequestBody ProjectVO projectVO) {
+    @ResponseBody
+    public String insertProject(@RequestBody ProjectVO projectVO) {
     	
     	int result = projectService.insertProject(projectVO);
-    	model.addAttribute("result", result);
     	
-    	return "main";
+    	return result > 0 ? "success" : "fail";
+    }
+    
+    @DeleteMapping("/{projectId}")
+    public String deleteProject(@PathVariable Long projectId) {
+    	
+    	int result = projectService.deleteProject(projectId);
+    	
+    	return result > 0 ? "success" : "fail";
     }
     
 }
