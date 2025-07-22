f5d72e7f7e731e538e0290523b500818cab3be54
Zino <jinho1203@gmail.com>
2025-07-22 15:37:05 +0900

    중간 커밋

--- src/main/java/egovframework/example/controller/MainController.java
+++ src/main/java/egovframework/example/controller/MainController.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,22 +31,32 @@ public class MainController {
     	
         return "main";  
     }
-    
+    /*
+     * Project 분류 및 등록
+     */
     @PostMapping("/project")
     @ResponseBody
     public String insertProject(@RequestBody ProjectVO projectVO) {
+        
+    	if (projectVO.getProjectName() == null || projectVO.getProjectName().trim().isEmpty()) {
+            return "fail";
+        }
     	
     	int result = projectService.insertProject(projectVO);
     	
     	return result > 0 ? "success" : "fail";
     }
     
-    @DeleteMapping("/{projectId}")
-    public String deleteProject(@PathVariable Long projectId) {
+    
+    @PostMapping("/project/delete")
+    public String deleteProject(@RequestBody List<Long> projectId) {
+    	for(Long id : projectId) {
+    		projectService.deleteProject(id);
+    	}
     	
-    	int result = projectService.deleteProject(projectId);
-    	
-    	return result > 0 ? "success" : "fail";
+    	return "success";
     }
+    
+    
     
 }
