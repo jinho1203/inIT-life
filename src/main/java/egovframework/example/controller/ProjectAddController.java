f6f3d34a7654e121bf95efcff1b0b22fe7bc8735
Zino <jinho1203@gmail.com>
2025-07-28 16:16:04 +0900

    프로젝트 추가페이지
    등록/삭제 완성

--- src/main/java/egovframework/example/controller/ProjectAddController.java
+++ src/main/java/egovframework/example/controller/ProjectAddController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import egovframework.example.service.ProjectService;
 import egovframework.example.vo.ProjectVO;
@@ -26,5 +30,29 @@ public class ProjectAddController {
 		return "main/projectAdd";
 	}
 	
+	@PostMapping
+	@ResponseBody
+	public String insertProject(@RequestBody ProjectVO projectVO) {
+        
+		System.out.println("받은 프로젝트명: " + projectVO.getProjectName());
+        System.out.println("받은 설명: " + projectVO.getProjectExplain());
+        if (projectVO.getProjectName() == null || projectVO.getProjectName().trim().isEmpty()) {
+            return "fail";
+        }
+        
+        projectService.insertProject(projectVO);
+        
+        return "success";
+	}
 	
+	@DeleteMapping
+	@ResponseBody
+	public String deleteProject(@RequestBody List<Long> projectId) {
+    	
+		for(Long id : projectId) {
+    		projectService.deleteProject(id);
+    	}
+    	
+    	return "success";
+	}
 }
