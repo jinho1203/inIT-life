64a9d3cca338787abeba9f10ad0e3bfce329da1e
Zino <jinho1203@gmail.com>
2025-07-28 17:42:11 +0900

    그냥 커밋

--- src/main/java/egovframework/example/controller/ProjectConfigController.java
+++ src/main/java/egovframework/example/controller/ProjectConfigController.java
@@ -1,12 +1,17 @@
 package egovframework.example.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import egovframework.example.service.InputService;
 import egovframework.example.service.ProjectService;
 import egovframework.example.service.SourceService;
+import egovframework.example.vo.ProjectVO;
+import egovframework.example.vo.SourceVO;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -19,7 +24,10 @@ public class ProjectConfigController {
 	private final InputService inputService;
 	
 	@GetMapping
-	public String ShowSourceList() {
+	public String ShowSourceList(Model model) {
+		
+		List<SourceVO> sourceList = sourceService.getAllList();
+		model.addAttribute("sourceList", sourceList);
 		
 		return "main/projectConfig";
 	}
