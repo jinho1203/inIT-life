ce56fd1765472876e361e0c732dc831c6d272322
이종진 <dlwhdwls778@naver.com>
2025-07-29 14:51:54 +0900

    [이종진]  - 에로홈페이지 작성 / 이미지 폴더 3개 추가 / 부트스트랩 활용에 해당하는 CSS 변경

--- src/main/java/egovframework/example/controller/TestController.java
+++ src/main/java/egovframework/example/controller/TestController.java
@@ -4,26 +4,12 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import egovframework.example.mapper.TestMapper;
-import egovframework.example.vo.TestVO;
-
+	
 @Controller
 public class TestController {
-	private final TestMapper testMapper;
-	
-	public TestController (TestMapper testMapper) {
-		this.testMapper = testMapper;
-	}
-	
-//	@GetMapping("/test")
-//	public List<TestVO> getData() {
-//	    return testMapper.findAll();
-//	}
-	
-	@GetMapping("/test")
+	@GetMapping("error")
 	public String hello() {
-		return "test";
+		return "error";
 	}
 	
 	
