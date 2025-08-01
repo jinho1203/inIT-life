93b3e6098108d2ca55385deeeb28c372d9787d31
이종진 <dlwhdwls778@naver.com>
2025-08-01 14:10:43 +0900

    [이종진] - 실행 홈페이지 ... 버튼 실행 구현

--- src/main/java/egovframework/example/controller/ProjectStartController.java
+++ src/main/java/egovframework/example/controller/ProjectStartController.java
@@ -1,12 +1,16 @@
 package egovframework.example.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import egovframework.example.service.InputService;
+
 import egovframework.example.service.ProjectService;
 import egovframework.example.service.SourceService;
+import egovframework.example.vo.ProjectVO;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -14,8 +18,17 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/projectStart")
 public class ProjectStartController {
 	
+	private final ProjectService projectService;
+	private final SourceService sourceService;
+	
 	@GetMapping
-	public String ShowSourceList() {
+	public String ShowSourceList(Model model) {
+		
+		List<ProjectVO> projectList = projectService.getAllList();
+//    	List<SourceVO> sourceList = sourceService.getAllList();
+    	
+    	model.addAttribute("projectList", projectList);
+//    	model.addAttribute("sourceList", sourceList);
 		
 		return "main/projectStart";
 	}
